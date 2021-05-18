3c8bcd94056f5a0ef1f2bef8be8fa2fc9161d696
Jimmy Ben Klieve <j.b.klieve@live.cn>
2021-05-18 16:24:06 +0800

    style: change indetation to 2 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@
 // - https://github.com/ant-tool/atool-build#配置扩展
 
 module.exports = function (webpackConfig) {
-    webpackConfig.babel.plugins.push('transform-runtime');
-    webpackConfig.babel.plugins.push(['import', {
-        libraryName: 'antd',
-        style: 'css',
-    }]);
+  webpackConfig.babel.plugins.push('transform-runtime');
+  webpackConfig.babel.plugins.push(['import', {
+    libraryName: 'antd',
+    style: 'css',
+  }]);
 
-    return webpackConfig;
+  return webpackConfig;
 };
